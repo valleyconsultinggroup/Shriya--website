b5c927d897147c96e1b145fd6ce53cfd72d04bf9
Shriya Nandwani <shriya.nandwani@gmail.com>
2018-11-15 17:00:53 -0800

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 // typing
 var TxtRotate = function(el, toRotate, period) {
@@ -107,21 +103,6 @@ window.onload = function() {
 			.scrolly({
 				speed: 1500,
 				offset: $header.outerHeight()
-			});
-
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
 			});
 
 	// Header.
